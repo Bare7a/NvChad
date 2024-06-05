a986ad4279881050cc68c685cfea7e974cb01a7d
Bare7a <bare7a@gmail.com>
2024-06-05 04:31:47 +0300

    Bare7a - Initial changes

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,14 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+map("n", "<A-up>", ":wincmd k<CR>")
+map("n", "<A-down>", ":wincmd j<CR>")
+map("n", "<A-left>", ":wincmd h<CR>")
+map("n", "<A-right>", ":wincmd l<CR>")
+
+for i = 1, 9, 1 do
+  map("n", string.format("<A-%s>", i), function()
+    vim.api.nvim_set_current_buf(vim.t.bufs[i])
+  end)
+end
