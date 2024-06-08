0ab22e43c139c924c21192452eb773b4021ffdf9
Bare7a <bare7a@gmail.com>
2024-06-08 21:57:13 +0300

    Adds vim-fugitive

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -52,7 +52,6 @@ return {
     config = function()
       require("noice").setup {
         lsp = {
-          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
           override = {
             ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
             ["vim.lsp.util.stylize_markdown"] = true,
@@ -65,15 +64,18 @@ return {
             enabled = false,
           },
         },
-        -- you can enable a preset for easier configuration
         presets = {
-          bottom_search = true, -- use a classic bottom cmdline for search
-          command_palette = true, -- position the cmdline and popupmenu together
-          long_message_to_split = true, -- long messages will be sent to a split
-          inc_rename = false, -- enables an input dialog for inc-rename.nvim
-          lsp_doc_border = false, -- add a border to hover docs and signature help
+          bottom_search = true,
+          command_palette = true,
+          long_message_to_split = true,
+          inc_rename = false,
+          lsp_doc_border = false,
         },
       }
     end,
+  },
+  {
+    "tpope/vim-fugitive",
+    event = "BufWinEnter",
   },
 }
